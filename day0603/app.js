63a0ad6ece5c488a4ed45a37213b623d7b75e8e8
rohannimap <rohan.hakke@nimapinfotech.com>
2025-03-06 18:59:45 +0530

    Add files via upload

---
+++ day0603/app.js
@@ -0,0 +1,103 @@
+// console.log("hello one two three");
+
+// console.log(`
+//     hwllo
+//     new`);
+
+// let username = "anurag";
+// console.log(typeof username);
+// let a;
+// console.log(typeof a);
+// a =2
+// console.log(typeof a);
+
+// execution phase of code
+//memory creation phase
+
+
+//code execution phase
+// console.log(a);
+
+// var a = 10
+// tempral dead zoon ->
+
+
+
+//templete literal
+// let a ="hello i am rohan"
+// console.log(a[0]);
+
+// console.log(a.length);
+// console.log(a.charAt(0));
+// console.log(a.toUpperCase());
+// console.log(a);
+// console.log(a.includes("am"));
+
+
+// math Object
+// console.log(Math.PI); // 3.141592653589793
+// console.log(Math.abs(-10)); // 10
+// console.log(Math.ceil(4.2)); // 5
+// console.log(Math.floor(4.7)); // 4
+// console.log(Math.round(4.5)); // 5
+// console.log(Math.max(1, 2, 3)); // 3
+// console.log(Math.min(1, 2, 3)); // 1
+// console.log(Math.sqrt(16)); // 4
+// console.log(Math.pow(2, 3)); // 8
+// console.log(Math.random()); // Random number between 0 and 1
+// for(let i =0;i<10;i++){
+
+//     setTimeout(
+//         function(){
+
+//             console.log(Math.random())
+//         }
+//         ,i*1000 )
+   
+// }
+
+
+// // Nested Ternary Operators
+// let score = 50;
+// let grade = (score >= 90) ? 'A' :
+//             (score >= 80) ? 'B' :
+//             (score >= 70) ? 'C' :
+//             (score >= 60) ? 'D' : 'F';
+
+// console.log(grade);
+
+// // logical operatror
+// let a = true;
+// let b = false;
+
+// console.log(a && b); 
+// console.log(a && true); 
+
+
+// //bitwise operator
+// let a = 5;  // Binary: 0101
+// let b = 3;  // Binary: 0011
+// let result = a & b; // Binary: 0001 (Decimal: 1)
+// console.log(result); // Output: 1
+// let a = 5;  // Binary: 0101
+// let b = 3;  // Binary: 0011
+// let result = a | b; // Binary: 0111 (Decimal: 7)
+// console.log(result); // Output: 7
+
+
+// itwise XOR (^)
+// only if same oo and 11 is tehre then output is the 0
+
+// Bitwise NOT (~)  
+// conevt if 1 then 0 and of 0 then 1
+
+// function removeDuplicates(arr) {
+//     return [...new Set(arr)];
+// }
+// // Example
+// console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Output: [1, 2, 3, 4, 5]
+
+console.log(7 > 2 && 5 > 4)
+
+
+
