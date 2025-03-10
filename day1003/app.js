b7bc469aaface6cd71600fc9de56ca7b6d750bfa
rohannimap <rohan.hakke@nimapinfotech.com>
2025-03-10 18:02:00 +0530

    Add files via upload

---
+++ day1003/app.js
@@ -0,0 +1,60 @@
+// const frouts = ["apple","banana","dates",[1,2,3,4,5]]
+// const animal = ["cat","dog","rat"]
+// // console.log(typeof frouts);
+// frouts[2]= "orange"
+// const conca = frouts.concat(animal)
+// // console.log(frouts[3][2]);
+// // console.log(frouts[3][2]);
+// console.log(frouts);
+// console.log(conca);
+
+// const userName = "rohan"
+// const userAge = 20
+// function seyhii(){
+//     console.log("hii");
+    
+
+// }
+
+
+// //HERE a IS THE high order function
+// function a(c){
+//     console.dir(c);
+//     c()
+// }
+// // a("hii")
+// // a({name:"rohan",num :12})
+// // a([1,2,3,4,5,6])
+// // console.dir(a)
+
+
+// function b(){
+//     console.log("test");
+// }
+// // in that a in b function paased that b funtion is call ballback function
+// a(b)
+
+// let a = 10
+// setTimeout(()=>{
+//     console.log("hello");
+    
+// },2000
+// )
+
+const a= [1,2,3,4,5]
+let b =  a.map((c)=>{
+    // console.log(c);
+    return c
+})
+console.log(b);
+
+
+
+
+
+
+
+
+// function myFun(){
+//         return 10
+// }
