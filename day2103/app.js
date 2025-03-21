7e3269ea73eefdb6cbf6a1e0e1b04131eca9ad58
rohannimap <rohan.hakke@nimapinfotech.com>
2025-03-21 12:20:43 +0530

    Add files via upload

---
+++ day2103/app.js
@@ -0,0 +1,58 @@
+// async function a (){
+//     var i = 1;
+// await new Promise((resolve)=>{
+//     setTimeout(()=>{
+//         resolve(console.log(i)) 
+//         resolve()
+//      },3000);
+
+// })
+//     console.log("hello");
+    
+// }
+// a()
+// // ----------------------
+// function a(){
+// for(var  i = 0;i <=4;i++){
+//     setTimeout(function(){
+//         console.log(i);
+//     })
+// }
+// } // 55555
+// a()
+// ______________________________
+// function a(){
+//     for(var i = 0;i<=4;i++){
+//         function b(i){
+//             setTimeout(() => {
+//                 console.log(i);
+                
+//             }, i*1000);
+//         }
+//         b(i)
+//     }
+// }
+// a() //01234
+// ___________________________
+// var a = 7;
+// b()
+// c()
+// console.log(a);
+// function b(){
+//     var a = 10
+//     console.log(a);
+// }
+// function c(){
+//     var a = 100
+//     console.log(a);
+// }
+// -----------------------
+
+
+
+
+
+
+
+
+
---
+++ day2103/test.js
@@ -0,0 +1,22 @@
+// var x = 7
+// function name(){
+//     console.log("hello");
+    
+// }
+
+// name()
+// console.log(x);
+// console.log(name);
+// -------------------------------
+a()
+console.log(b);
+function a(){
+    var b = 10;
+    c()
+    function c(){
+    }
+        
+    }
+
+
+
