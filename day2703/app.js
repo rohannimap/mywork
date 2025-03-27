47cb23802a29c42bc5557d0aae7ee99e412beccb
rohannimap <rohan.hakke@nimapinfotech.com>
2025-03-27 18:59:44 +0530

    Add files via upload

---
+++ day2703/app.js
@@ -0,0 +1,90 @@
+// function str(n){
+//     for(let i = 0;i<5;i++){
+//         let a = ''
+//         for(let j = 0;j<i;j++){
+//             a+="*"
+            
+//         }
+        
+//         console.log(a);
+//     }
+// }
+// str(5)
+// -------------------------------
+// function max(arr){
+//     let n = arr[0];
+//     for(let i = 1;i<arr.length;i++){
+//             if(arr[i]>n){
+//                 n =arr[i]
+//             }
+//     }
+//     console.log(n);
+// }
+//  max([52,2,9,4,11]);
+//  --------------------------------
+// function sum(arr){
+// let su = 0;
+// for(let i of arr){
+//     su+=i
+// }
+// console.log(su);
+// }
+// sum([1,2,3,4,5,6,7,8,9,10])
+// or
+// function sum(arr){
+//     let su= 0;
+//     arr.map((n)=>su+=n);
+// console.log(su);
+// }
+// sum([1,2,3,4,5])
+// or
+// function sum(arr){
+//     let su= 0;
+//    arr.forEach(element => {
+//    su+=element
+//    });
+// console.log(su);
+// }
+// sum([1,2,3,4,5])
+// ----------------------------------
+// console.log( 1 + "1"); //11
+// console.log([1 ,2] + "1" ); // "1,21"
+// console.log(true + "1");  true1
+// --------------------
+// function  sec(arr){
+//     arr.sort((a,b)=>b-a)
+//     console.log(arr[1]);
+// }
+// sec([11,6,3,4,5])
+// ----------------------
+// function test( )
+// {
+//     let a=2
+//     function demo(){
+//         let b =4
+//         function check(){
+//             let c =6
+//         }
+//         check()
+//     }
+// }
+// test()
+// ----------------------------
+// let a = [1,2,3,4,5,6]     //passs array in the arrow function
+// let test = (b)=>{
+// for(let i = 0 ;i <5;i++){
+//     console.log(b[i]);
+// }
+// }
+// test(a)
+// ------------------------------
+
+
+
+
+
+
+
+
+
+
---
+++ day2703/test.js
@@ -0,0 +1,24 @@
+// console.log("Start");
+// setTimeout(() => console.log("Timeout"), 0);
+// Promise.resolve().then(() => console.log("Promise resolved"));
+// console.log("End");
+
+
+
+// let arr = [1,2,3,4,5,6]
+// arr.forEach((a)=>{
+//     a+2
+// })
+// arr.map()
+
+
+let a = 9
+function test(){
+    {
+
+        
+        console.log(a);
+    }
+
+}
+test()
